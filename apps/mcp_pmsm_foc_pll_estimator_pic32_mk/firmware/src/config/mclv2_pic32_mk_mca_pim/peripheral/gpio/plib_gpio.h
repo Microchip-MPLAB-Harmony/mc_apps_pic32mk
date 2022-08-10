f603faaf768f99f6e3fc75b194565d2c2bcf5f53
Binod Sharma <Binod.Sharma@microchip.com>
2022-08-10 18:21:17 +0530

    [Apps] Adding MCP generated code for PLL based sensorless FOC with PIC32MK MCA on dsPICDEM MCLV2
    MH3-63773

---
+++ apps/mcp_pmsm_foc_pll_estimator_pic32_mk/firmware/src/config/mclv2_pic32_mk_mca_pim/peripheral/gpio/plib_gpio.h
@@ -0,0 +1,332 @@
+/*******************************************************************************
+  GPIO PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_gpio.h UUUUUUUUU
+
+  Summary:
+    GPIO PLIB Header File
+
+  Description:
+    This library provides an interface to control and interact with Parallel
+    Input/Output controller (GPIO) module.
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#ifndef PLIB_GPIO_H
+#define PLIB_GPIO_H
+
+#include <device.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data types and constants
+// *****************************************************************************
+// *****************************************************************************
+
+
+/*** Macros for LED_02 pin ***/
+#define LED_02_Set()               (LATASET = (1U<<7))
+#define LED_02_Clear()             (LATACLR = (1U<<7))
+#define LED_02_Toggle()            (LATAINV= (1U<<7))
+#define LED_02_OutputEnable()      (TRISACLR = (1U<<7))
+#define LED_02_InputEnable()       (TRISASET = (1U<<7))
+#define LED_02_Get()               ((PORTA >> 7) & 0x1U)
+#define LED_02_PIN                  GPIO_PIN_RA7
+
+/*** Macros for PWM_AH pin ***/
+#define PWM_AH_Get()               ((PORTB >> 14) & 0x1U)
+#define PWM_AH_PIN                  GPIO_PIN_RB14
+
+/*** Macros for PWM_AL pin ***/
+#define PWM_AL_Get()               ((PORTB >> 15) & 0x1U)
+#define PWM_AL_PIN                  GPIO_PIN_RB15
+
+/*** Macros for BUTTON_01 pin ***/
+#define BUTTON_01_Set()               (LATASET = (1U<<12))
+#define BUTTON_01_Clear()             (LATACLR = (1U<<12))
+#define BUTTON_01_Toggle()            (LATAINV= (1U<<12))
+#define BUTTON_01_OutputEnable()      (TRISACLR = (1U<<12))
+#define BUTTON_01_InputEnable()       (TRISASET = (1U<<12))
+#define BUTTON_01_Get()               ((PORTA >> 12) & 0x1U)
+#define BUTTON_01_PIN                  GPIO_PIN_RA12
+
+/*** Macros for IA pin ***/
+#define IA_Get()               ((PORTA >> 4) & 0x1U)
+#define IA_PIN                  GPIO_PIN_RA4
+
+/*** Macros for LED_01 pin ***/
+#define LED_01_Set()               (LATDSET = (1U<<8))
+#define LED_01_Clear()             (LATDCLR = (1U<<8))
+#define LED_01_Toggle()            (LATDINV= (1U<<8))
+#define LED_01_OutputEnable()      (TRISDCLR = (1U<<8))
+#define LED_01_InputEnable()       (TRISDSET = (1U<<8))
+#define LED_01_Get()               ((PORTD >> 8) & 0x1U)
+#define LED_01_PIN                  GPIO_PIN_RD8
+
+/*** Macros for VDC pin ***/
+#define VDC_Get()               ((PORTC >> 10) & 0x1U)
+#define VDC_PIN                  GPIO_PIN_RC10
+
+/*** Macros for VPOT pin ***/
+#define VPOT_Get()               ((PORTB >> 7) & 0x1U)
+#define VPOT_PIN                  GPIO_PIN_RB7
+
+/*** Macros for BUTTON_02 pin ***/
+#define BUTTON_02_Set()               (LATCSET = (1U<<13))
+#define BUTTON_02_Clear()             (LATCCLR = (1U<<13))
+#define BUTTON_02_Toggle()            (LATCINV= (1U<<13))
+#define BUTTON_02_OutputEnable()      (TRISCCLR = (1U<<13))
+#define BUTTON_02_InputEnable()       (TRISCSET = (1U<<13))
+#define BUTTON_02_Get()               ((PORTC >> 13) & 0x1U)
+#define BUTTON_02_PIN                  GPIO_PIN_RC13
+
+/*** Macros for RECEIVE pin ***/
+#define RECEIVE_Get()               ((PORTB >> 8) & 0x1U)
+#define RECEIVE_PIN                  GPIO_PIN_RB8
+
+/*** Macros for IB pin ***/
+#define IB_Get()               ((PORTB >> 9) & 0x1U)
+#define IB_PIN                  GPIO_PIN_RB9
+
+/*** Macros for QEA pin ***/
+#define QEA_Get()               ((PORTC >> 7) & 0x1U)
+#define QEA_PIN                  GPIO_PIN_RC7
+
+/*** Macros for QEB pin ***/
+#define QEB_Get()               ((PORTC >> 8) & 0x1U)
+#define QEB_PIN                  GPIO_PIN_RC8
+
+/*** Macros for TRANSMIT pin ***/
+#define TRANSMIT_Get()               ((PORTC >> 9) & 0x1U)
+#define TRANSMIT_PIN                  GPIO_PIN_RC9
+
+/*** Macros for PWM_CH pin ***/
+#define PWM_CH_Get()               ((PORTB >> 10) & 0x1U)
+#define PWM_CH_PIN                  GPIO_PIN_RB10
+
+/*** Macros for PWM_CL pin ***/
+#define PWM_CL_Get()               ((PORTB >> 11) & 0x1U)
+#define PWM_CL_PIN                  GPIO_PIN_RB11
+
+/*** Macros for PWM_BH pin ***/
+#define PWM_BH_Get()               ((PORTB >> 12) & 0x1U)
+#define PWM_BH_PIN                  GPIO_PIN_RB12
+
+/*** Macros for PWM_BL pin ***/
+#define PWM_BL_Get()               ((PORTB >> 13) & 0x1U)
+#define PWM_BL_PIN                  GPIO_PIN_RB13
+
+
+// *****************************************************************************
+/* GPIO Port
+
+  Summary:
+    Identifies the available GPIO Ports.
+
+  Description:
+    This enumeration identifies the available GPIO Ports.
+
+  Remarks:
+    The caller should not rely on the specific numbers assigned to any of
+    these values as they may change from one processor to the next.
+
+    Not all ports are available on all devices.  Refer to the specific
+    device data sheet to determine which ports are supported.
+*/
+
+
+#define    GPIO_PORT_A  (0)
+#define    GPIO_PORT_B  (1)
+#define    GPIO_PORT_C  (2)
+#define    GPIO_PORT_D  (3)
+typedef uint32_t GPIO_PORT;
+
+typedef enum
+{
+    GPIO_INTERRUPT_ON_MISMATCH,
+    GPIO_INTERRUPT_ON_RISING_EDGE,
+    GPIO_INTERRUPT_ON_FALLING_EDGE,
+    GPIO_INTERRUPT_ON_BOTH_EDGES,
+}GPIO_INTERRUPT_STYLE;
+
+// *****************************************************************************
+/* GPIO Port Pins
+
+  Summary:
+    Identifies the available GPIO port pins.
+
+  Description:
+    This enumeration identifies the available GPIO port pins.
+
+  Remarks:
+    The caller should not rely on the specific numbers assigned to any of
+    these values as they may change from one processor to the next.
+
+    Not all pins are available on all devices.  Refer to the specific
+    device data sheet to determine which pins are supported.
+*/
+
+
+#define     GPIO_PIN_RA0  (0U)
+#define     GPIO_PIN_RA1  (1U)
+#define     GPIO_PIN_RA4  (4U)
+#define     GPIO_PIN_RA7  (7U)
+#define     GPIO_PIN_RA8  (8U)
+#define     GPIO_PIN_RA10  (10U)
+#define     GPIO_PIN_RA11  (11U)
+#define     GPIO_PIN_RA12  (12U)
+#define     GPIO_PIN_RB0  (16U)
+#define     GPIO_PIN_RB1  (17U)
+#define     GPIO_PIN_RB2  (18U)
+#define     GPIO_PIN_RB3  (19U)
+#define     GPIO_PIN_RB4  (20U)
+#define     GPIO_PIN_RB5  (21U)
+#define     GPIO_PIN_RB6  (22U)
+#define     GPIO_PIN_RB7  (23U)
+#define     GPIO_PIN_RB8  (24U)
+#define     GPIO_PIN_RB9  (25U)
+#define     GPIO_PIN_RB10  (26U)
+#define     GPIO_PIN_RB11  (27U)
+#define     GPIO_PIN_RB12  (28U)
+#define     GPIO_PIN_RB13  (29U)
+#define     GPIO_PIN_RB14  (30U)
+#define     GPIO_PIN_RB15  (31U)
+#define     GPIO_PIN_RC0  (32U)
+#define     GPIO_PIN_RC1  (33U)
+#define     GPIO_PIN_RC2  (34U)
+#define     GPIO_PIN_RC6  (38U)
+#define     GPIO_PIN_RC7  (39U)
+#define     GPIO_PIN_RC8  (40U)
+#define     GPIO_PIN_RC9  (41U)
+#define     GPIO_PIN_RC10  (42U)
+#define     GPIO_PIN_RC11  (43U)
+#define     GPIO_PIN_RC12  (44U)
+#define     GPIO_PIN_RC13  (45U)
+#define     GPIO_PIN_RC15  (47U)
+#define     GPIO_PIN_RD8  (56U)
+
+    /* This element should not be used in any of the GPIO APIs.
+       It will be used by other modules or application to denote that none of the GPIO Pin is used */
+#define    GPIO_PIN_NONE   (-1)
+
+typedef uint32_t GPIO_PIN;
+
+
+void GPIO_Initialize(void);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO Functions which operates on multiple pins of a port
+// *****************************************************************************
+// *****************************************************************************
+
+uint32_t GPIO_PortRead(GPIO_PORT port);
+
+void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value);
+
+uint32_t GPIO_PortLatchRead ( GPIO_PORT port );
+
+void GPIO_PortSet(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortClear(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortToggle(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO Functions which operates on one pin at a time
+// *****************************************************************************
+// *****************************************************************************
+
+static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
+{
+	 uint32_t xvalue = (uint32_t)value;
+    GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
+}
+
+static inline bool GPIO_PinRead(GPIO_PIN pin)
+{
+    return ((((GPIO_PortRead((GPIO_PORT)(pin>>4U))) >> (pin & 0xFU)) & 0x1U) != 0U);
+}
+
+static inline bool GPIO_PinLatchRead(GPIO_PIN pin)
+{
+    return (((GPIO_PortLatchRead((GPIO_PORT)(pin>>4U)) >> (pin & 0xFU)) & 0x1U) != 0U);
+}
+
+static inline void GPIO_PinToggle(GPIO_PIN pin)
+{
+    GPIO_PortToggle((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinSet(GPIO_PIN pin)
+{
+    GPIO_PortSet((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinClear(GPIO_PIN pin)
+{
+    GPIO_PortClear((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinInputEnable(GPIO_PIN pin)
+{
+    GPIO_PortInputEnable((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinOutputEnable(GPIO_PIN pin)
+{
+    GPIO_PortOutputEnable((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    }
+
+#endif
+// DOM-IGNORE-END
+#endif // PLIB_GPIO_H

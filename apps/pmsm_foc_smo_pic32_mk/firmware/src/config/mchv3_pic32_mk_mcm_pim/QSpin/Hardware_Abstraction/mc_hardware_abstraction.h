60f8231496b2f4cb9c048d43eb31f00fd0ce6c96
Binod Sharma <Binod.Sharma@microchip.com>
2023-07-14 17:14:53 +0530

    [DFT] DFT Regeneration and test for pmsm_foc_smo_pic32_mk_mchv3_pic32_mk_mcm_pim MH3-81565

---
+++ apps/pmsm_foc_smo_pic32_mk/firmware/src/config/mchv3_pic32_mk_mcm_pim/QSpin/Hardware_Abstraction/mc_hardware_abstraction.h
@@ -0,0 +1,335 @@
+/*******************************************************************************
+  System Definitions
+
+  File Name:
+    mc_hardware_abstraction.h
+
+  Summary:
+    Header file which shares global variables and function prototypes.
+
+  Description:
+    This file contains the global variables and function prototypes for hardware abstraction.
+
+ *******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2021 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef MCHAL_H
+#define MCHAL_H
+
+
+/*******************************************************************************
+  Header inclusions  
+*******************************************************************************/
+#include "mc_types.h"
+#include "definitions.h"
+
+/*******************************************************************************
+ * Interface variables
+*******************************************************************************/
+/** Analog interfaces */
+extern uint16_t mcHalI_IaAdcInput_gdu16;
+extern uint16_t mcHalI_IbAdcInput_gdu16;
+extern uint16_t mcHalI_UbusAdcInput_gdu16;
+extern uint16_t mcHalI_Potentiometer_gdu16;
+extern int16_t mcPwmI_Duty_gau16[3u];
+
+/*******************************************************************************
+ * User defined data structure 
+*******************************************************************************/
+
+/*******************************************************************************
+ * Static interface Functions
+*******************************************************************************/
+
+/*! \brief Get PWM period
+ *
+ * Details.
+ * Get PWM period
+ *
+ * @param[in]:
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_INLINE uint16_t mcHalI_PwmPeriodGet( void )
+{
+    return (uint16_t)MCPWM_PrimaryPeriodGet();
+}
+
+/*! \brief Inverter Duty Set
+ * 
+ * Details.
+ * Sets the PWM inverter duty
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_InverterPwmSet( const int16_t * const dutyCycle )
+{
+    MCPWM_ChannelPrimaryDutySet(MCPWM_CH_1, (uint16_t)dutyCycle[0] );
+    MCPWM_ChannelPrimaryDutySet(MCPWM_CH_2, (uint16_t)dutyCycle[1] );
+    MCPWM_ChannelPrimaryDutySet(MCPWM_CH_3, (uint16_t)dutyCycle[2] );
+}
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_PhaseACurrentGet( void )
+{
+    mcHalI_IaAdcInput_gdu16 = ADCHS_ChannelResultGet(ADCHS_CH3);
+}
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_PhaseBCurrentGet( void )
+{
+    mcHalI_IbAdcInput_gdu16 =  ADCHS_ChannelResultGet(ADCHS_CH1);
+}
+
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_DcLinkVoltageGet( void )
+{
+    /** Get ADC value for DC bus voltage */
+    mcHalI_UbusAdcInput_gdu16 = ADCHS_ChannelResultGet(ADCHS_CH10);
+}
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_PotentiometerInputGet( void )
+{
+    /** Get ADC value for DC bus voltage */
+    mcHalI_Potentiometer_gdu16 = ADCHS_ChannelResultGet(ADCHS_CH15);
+}
+
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_AdcInterruptClear( void )
+{
+    EVIC_SourceStatusClear(INT_SOURCE_ADC_DATA3);
+}
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_AdcInterruptDisable( void )
+{
+    EVIC_SourceDisable(INT_SOURCE_ADC_DATA3);
+}
+
+/*! \brief Get analog signals from ADC peripheral
+ * 
+ * Details.
+ * Get analog signals from ADC peripheral
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+__STATIC_FORCEINLINE void mcHalI_AdcInterruptEnable( void )
+{
+    EVIC_SourceEnable(INT_SOURCE_ADC_DATA3);
+}
+
+
+/*******************************************************************************
+ * Interface Functions
+*******************************************************************************/
+
+/*! \brief Enable PWM inverter
+ * 
+ * Details.
+ * Enable PWM inverter
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+void mcHalI_InverterPwmEnable( void );
+
+/*! \brief Disable PWM inverter
+ * 
+ * Details.
+ * Disable PWM inverter
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+void mcHalI_InverterPwmDisable( void );
+
+
+        /*! \brief Set fault indicator
+		 * 
+		 * Details.
+		 * Set fault indicator
+		 * 
+		 * @param[in]: 
+		 * @param[in/out]:
+		 * @param[out]:
+		 * @return:
+		 */
+        void mcHal_FaultIndicationSet( void );
+        
+       
+		 /*! \brief Set direction indicator
+		 * 
+		 * Details.
+		 * Set direction indicator
+		 * 
+		 * @param[in]: 
+		 * @param[in/out]:
+		 * @param[out]:
+		 * @return:
+		 */
+         void mcHal_DirectionIndication( void );
+         
+       
+
+
+/*! \brief ADC Enable
+ * 
+ * Details.
+ * ADC Enable
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+void mcHalI_AdcEnable( void );
+
+
+/*! \brief PWM timer Start
+ * 
+ * Details.
+ * PWM timer Start
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+void mcHalI_PwmTimerStart( void );
+
+/*! \brief ADC callback function
+ * 
+ * Details.
+ * ADC callback function
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+void mcHalI_AdcCallBackRegister( ADCHS_CALLBACK callback, uintptr_t context );
+
+/*! \brief PWM fault callback function
+ * 
+ * Details.
+ * PWM fault callback function
+ * 
+ * @param[in]: 
+ * @param[in/out]:
+ * @param[out]:
+ * @return:
+ */
+void mcHalI_PwmCallbackRegister( MCPWM_CH_CALLBACK callback, uintptr_t context );
+
+
+ /*! \brief PWM interrupt enable
+  *
+  * Details.
+  * PWM interrupt enable
+  *
+  * @param[in]:
+  * @param[in/out]:
+  * @param[out]:
+  * @return:
+  */
+ void mcHalI_PwmInterruptEnable( void );
+
+
+bool mcHalI_StartStopButtonState( void );
+
+
+
+
+#endif // MCHAL_H
---
+++ apps/pmsm_foc_smo_pic32_mk/firmware/src/config/mchv3_pic32_mk_mcm_pim/peripheral/adchs/plib_adchs.c
@@ -0,0 +1,275 @@
+/*******************************************************************************
+  ADCHS Peripheral Library Interface Source File
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_adchs.c
+
+  Summary
+    ADCHS peripheral library source.
+
+  Description
+    This file implements the ADCHS peripheral library.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+#include "device.h"
+#include "plib_adchs.h"
+#include "interrupts.h"
+
+#define ADCHS_CHANNEL_32  (32U)
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: ADCHS Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+/* Object to hold callback function and context */
+volatile static ADCHS_CALLBACK_OBJECT ADCHS_CallbackObj[53];
+
+
+
+void ADCHS_Initialize(void)
+{
+    ADCCON1bits.ON = 0;
+    ADC1CFG = DEVADC1;
+    ADC1TIME = 0x3010001U;
+    ADC3CFG = DEVADC3;
+    ADC3TIME = 0x3010001U;
+
+    ADC7CFG = DEVADC7;
+
+    ADCCON1 = 0x600000U;
+    ADCCON2 = 0x20001U;
+    ADCCON3 = 0x1000000U;
+
+    ADCTRGMODE = 0x840000U;
+
+    ADCTRG1 = 0xa000a0aU; 
+    ADCTRG2 = 0xaU; 
+    ADCTRG3 = 0xa0000U; 
+    ADCTRG4 = 0xa000000U; 
+    ADCTRG5 = 0x0U; 
+    ADCTRG6 = 0x0U; 
+    ADCTRG7 = 0x0U; 
+
+    ADCTRGSNS = 0x0U;
+
+    ADCIMCON1 = 0x0U;
+    ADCIMCON2 = 0x0U; 
+    ADCIMCON3 = 0x0U; 
+    ADCIMCON4 = 0x0U; 
+
+    /* Input scan */
+    ADCCSS1 = 0x0U;
+    ADCCSS2 = 0x0U; 
+
+
+
+
+/* Result interrupt enable */
+ADCGIRQEN1 = 0x8U;
+ADCGIRQEN2 = 0x0U;
+/* Interrupt Enable */
+IEC3SET = 0x2000U;
+IEC4SET = 0x0U;
+
+
+
+    /* Turn ON ADC */
+    ADCCON1bits.ON = 1;
+    while(ADCCON2bits.BGVRRDY == 0U) // Wait until the reference voltage is ready
+    {
+        /* Nothing to do */
+    }
+    while(ADCCON2bits.REFFLT != 0U) // Wait if there is a fault with the reference voltage
+    {
+        /* Nothing to do */
+    }
+
+    /* ADC 1 */
+    ADCANCONbits.ANEN1 = 1;      // Enable the clock to analog bias
+    while(ADCANCONbits.WKRDY1 == 0U) // Wait until ADC is ready
+    {
+        /* Nothing to do */
+    }
+    ADCCON3bits.DIGEN1 = 1;      // Enable ADC
+
+    /* ADC 3 */
+    ADCANCONbits.ANEN3 = 1;      // Enable the clock to analog bias
+    while(ADCANCONbits.WKRDY3 == 0U) // Wait until ADC is ready
+    {
+        /* Nothing to do */
+    }
+    ADCCON3bits.DIGEN3 = 1;      // Enable ADC
+
+    /* ADC 7 */
+    ADCANCONbits.ANEN7 = 1;      // Enable the clock to analog bias
+    while(ADCANCONbits.WKRDY7 == 0U) // Wait until ADC is ready
+    {
+        /* Nothing to do */
+    }
+    ADCCON3bits.DIGEN7 = 1;      // Enable ADC
+
+
+}
+
+
+/* Enable ADCHS channels */
+void ADCHS_ModulesEnable (ADCHS_MODULE_MASK moduleMask)
+{
+    ADCCON3 |= ((uint32_t)moduleMask << 16);
+}
+
+/* Disable ADCHS channels */
+void ADCHS_ModulesDisable (ADCHS_MODULE_MASK moduleMask)
+{
+    ADCCON3 &= ~(((uint32_t)moduleMask << 16));
+}
+
+
+void ADCHS_ChannelResultInterruptEnable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCGIRQEN1 |= 0x01UL << channel;
+    }
+    else
+    {
+        ADCGIRQEN2 |= 0x01UL << (channel - 32U);
+    }
+}
+
+void ADCHS_ChannelResultInterruptDisable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCGIRQEN1 &= ~(0x01UL << channel);
+    }
+    else
+    {
+        ADCGIRQEN2 &= ~(0x01UL << (channel - 32U));
+    }
+}
+
+void ADCHS_ChannelEarlyInterruptEnable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCEIEN1 |= (0x01UL << channel);
+    }
+    else
+    {
+        ADCEIEN2 |= (0x01UL << (channel - 32U));
+    }
+}
+
+void ADCHS_ChannelEarlyInterruptDisable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCEIEN1 &= ~(0x01UL << channel);
+    }
+    else
+    {
+        ADCEIEN2 &= ~(0x01UL << (channel - 32U));
+    }
+}
+
+void ADCHS_GlobalEdgeConversionStart(void)
+{
+    ADCCON3bits.GSWTRG = 1;
+}
+
+void ADCHS_GlobalLevelConversionStart(void)
+{
+    ADCCON3bits.GLSWTRG = 1;
+}
+
+void ADCHS_GlobalLevelConversionStop(void)
+{
+    ADCCON3bits.GLSWTRG = 0;
+}
+
+void ADCHS_ChannelConversionStart(ADCHS_CHANNEL_NUM channel)
+{
+    ADCCON3bits.ADINSEL = (uint8_t)channel;
+    ADCCON3bits.RQCNVRT = 1;
+}
+
+
+/*Check if conversion result is available */
+bool ADCHS_ChannelResultIsReady(ADCHS_CHANNEL_NUM channel)
+{
+    bool status = false;
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        status = (((ADCDSTAT1 >> channel) & 0x01U) != 0U);
+    }
+    else
+    {
+        status = (((ADCDSTAT2 >> (channel - 32U)) & 0x01U) != 0U);
+    }
+    return status;
+}
+
+/* Read the conversion result */
+uint16_t ADCHS_ChannelResultGet(ADCHS_CHANNEL_NUM channel)
+{
+    return (uint16_t)(*((&ADCDATA0) + (channel << 2)));
+
+}
+
+void ADCHS_CallbackRegister(ADCHS_CHANNEL_NUM channel, ADCHS_CALLBACK callback, uintptr_t context)
+{
+    ADCHS_CallbackObj[channel].callback_fn = callback;
+    ADCHS_CallbackObj[channel].context = context;
+}
+
+
+
+
+bool ADCHS_EOSStatusGet(void)
+{
+    return (bool)(ADCCON2bits.EOSRDY);
+}
+
+void __attribute__((used)) ADC_DATA3_InterruptHandler(void)
+{
+    if (ADCHS_CallbackObj[3].callback_fn != NULL)
+    {
+        uintptr_t context = ADCHS_CallbackObj[3].context;
+        ADCHS_CallbackObj[3].callback_fn(ADCHS_CH3, context);
+    }
+
+
+    IFS3CLR = _IFS3_AD1D3IF_MASK;
+}
+
